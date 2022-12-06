57134ce26a09b2452255bfd0e986305da816419d
lilithovakimyan <lilithovakimian1998@gmail.com>
2022-12-06 22:45:10 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const url = useRef(null);
+  const [firstShortLink, setFirstShortLink] = useState("");
+  const [secondShortLink, setSecondShortLink] = useState("");
+
+  const handleRequest = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://api.shrtco.de/v2/shorten?url=example.org/very/long/link.html`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // setFirstShortLink(data.result.short_link);
+        console.log(data.result);
+        setFirstShortLink(data.result.short_link);
+        setSecondShortLink(data.result.short_link2);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleRequest}>
+        <input type="text" required ref={url} />
+        <input type="submit" value="shorten" />
+        <p>{firstShortLink && `Short url: ${firstShortLink}`}</p>
+        <p>{secondShortLink && `Short url 2: ${secondShortLink}`}</p>
+      </form>
     </div>
   );
 }
