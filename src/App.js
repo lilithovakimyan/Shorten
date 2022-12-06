6921a2145db20b26b35d1a6e0b20ce458e44f7bd
lilithovakimyan <lilithovakimian1998@gmail.com>
2022-12-06 22:48:12 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -8,13 +8,9 @@ function App() {
 
   const handleRequest = (e) => {
     e.preventDefault();
-    fetch(
-      `https://api.shrtco.de/v2/shorten?url=example.org/very/long/link.html`
-    )
+    fetch(`https://api.shrtco.de/v2/shorten?url=${url.current.value}`)
       .then((response) => response.json())
       .then((data) => {
-        // setFirstShortLink(data.result.short_link);
-        console.log(data.result);
         setFirstShortLink(data.result.short_link);
         setSecondShortLink(data.result.short_link2);
       });
